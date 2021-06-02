c055f4e96e12885c9f7da9e3bcd758103bee1e91
pleundv <84378584+pleundv@users.noreply.github.com>
2021-06-02 13:06:00 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,34 +1,30 @@
-const age = 12
+const age = 15;
+const isFemale = true;
+const driverStatus = "bob";
 
-if (age == 18) {
+
+if (age >= 18) {
 
     text = "Je gaat wel naar binnen";
 
-} else if (age > 18) {
-    text = "Je gaat wel naar binnen";
-} else if (age < 18) {
+} else {
     text = "Je gaat niet naar binnen";
 }
-
-
 console.log(text)
-
-const isFemale = true;
 
 if (isFemale) {
     text = "je bent een vrouw"
+} else {
+    text = " je bent een man"
 }
 
 console.log(text)
 
-const driverStatus = "bob";
-
-if (driverStatus == bob) {
-    text = "jij bent de bob"
+if (driverStatus === "bob") {
+    text = "jij mag rijden"
 } else {
     text = "je mag niet meer rijden"
 }
 
+
 console.log(text)
-
-
