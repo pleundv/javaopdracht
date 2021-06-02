7d0514851b1dd99446688318ae4826690a454988
pleundv <84378584+pleundv@users.noreply.github.com>
2021-06-02 12:53:47 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,34 @@
+const age = 12
+
+if (age == 18) {
+
+    text = "Je gaat wel naar binnen";
+
+} else if (age > 18) {
+    text = "Je gaat wel naar binnen";
+} else if (age < 18) {
+    text = "Je gaat niet naar binnen";
+}
+
+
+console.log(text)
+
+const isFemale = true;
+
+if (isFemale) {
+    text = "je bent een vrouw"
+}
+
+console.log(text)
+
+const driverStatus = "bob";
+
+if (driverStatus == bob) {
+    text = "jij bent de bob"
+} else {
+    text = "je mag niet meer rijden"
+}
+
+console.log(text)
+
+
